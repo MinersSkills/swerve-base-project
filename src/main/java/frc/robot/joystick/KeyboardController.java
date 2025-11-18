a0a2225a3d049715c0c7cdf37986cfed9d7ea0cb
Fernandes-Bernardo <bernardofpdev@gmail.com>
2025-11-18 09:31:33 -0300

    feat: implement trigger creator function

--- src/main/java/frc/robot/joystick/KeyboardController.java
+++ src/main/java/frc/robot/joystick/KeyboardController.java
@@ -1,5 +1,24 @@
 package frc.robot.joystick;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
 public class KeyboardController {
-    
+    private final NetworkTable table;
+
+    public KeyboardController(){
+        //Conect to the network table
+        table = NetworkTableInstance.getDefault().getTable("OperatorController");
+    }
+
+    private Trigger createTrigger(String key){
+        // function to read the key from table
+        return new Trigger(() -> table.getEntry(key).getBoolean(false));
+    }
+
+    // this is how to create a trigger to be read in a robot container
+    public Trigger getATrigger(){
+        return createTrigger("a");
+    }
 }
